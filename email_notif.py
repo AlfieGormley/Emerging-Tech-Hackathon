da26fd4f0b0ecd5254d4b775569fe1879293b385
EWAN <bealee1@cardiff.ac.uk>
2025-04-02 13:15:47 +0100

    added coucil notif

--- email_notif.py
+++ email_notif.py
@@ -2,34 +2,46 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# Email configuration
-sender_email = "ewanbeale2003@gmail.com"  # Your Gmail address
-receiver_email = "ewanbeale2nd@gmail.com"  # Receiver's email address
-password = "nbxz mgju zbcy kzab "  # Your Gmail password or app-specific password
-subject = "Test Email from Python"
-body = "Hello, this is a test email sent from Python."
 
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
+def main(score):
+    if score < 50:
+        creds = email_creds("councilhackaton@gmail.com","EXPENSIVE!!", "this bum using to much enrgy")
+        send_email(creds)
+    creds = email_creds("ewanbeale2nd@gmail.com", "paying too much!", "stop it ")
+    send_email(creds)
 
-# Add body to email
-message.attach(MIMEText(body, "plain"))
+def email_creds(receiver, sub, bdy):
+    receiver_email = receiver  # Receiver's email address
+    subject = sub
+    body = bdy
 
-# Send email using Gmail's SMTP server
-try:
-    # Set up the server (Gmail SMTP server)
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)  # Use port 465 for SSL
-    server.login(sender_email, password)  # Login to the server
-    
-    # Send the email
-    server.sendmail(sender_email, receiver_email, message.as_string())
-    print("Email sent successfully!")
+    return receiver_email, subject, body
 
-except Exception as e:
-    print(f"Error: {e}")
+def send_email(creds):
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = "ewanbeale203@gmail.com"
+    message["To"] = creds[0]
+    message["Subject"] = creds[1]
 
-finally:
-    server.quit()  # Close the connection to the server
+    # Add body to email
+    message.attach(MIMEText(creds[2], "plain"))
+
+    # Send email using Gmail's SMTP server
+    try:
+        # Set up the server (Gmail SMTP server)
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)  # Use port 465 for SSL
+        server.login("ewanbeale2003@gmail.com", "nbxz mgju zbcy kzab" )  # Login to the server
+        
+        # Send the email
+        server.sendmail("ewanbeale2003@gmail.com", creds[0], message.as_string())
+        print("Email sent successfully!")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        server.quit()  # Close the connection to the server
+
+if __name__ == "__main__":
+    main(40)
