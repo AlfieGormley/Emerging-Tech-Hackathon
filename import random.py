26572abd33a385b5c23e776c04c6cf16206a8e4d
AlfieGormley <alfiegyt@gmail.com>
2025-04-02 12:53:25 +0100

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime, timedelta
+import uuid
 from pymongo import MongoClient
 import datetime
 from faker import Faker
@@ -10,24 +11,6 @@ fake = Faker()
 client = MongoClient("mongodb://localhost:27017/")
 db = client["council"] #Database Name
 households_collection = db["households"]  #Collection name
-
-
-
-
-def Create_Household():
-
-    start_date = datetime(2025, 1, 1)
-    end_date = datetime(2025, 2, 1)
-    Date = fake.date_between(start_date=start_date, end_date=end_date)
-    Occupants = random.randint(1,6)#people
-    Gas = random.randint(700,1400)#Kw hours
-    Electric = random.randint(200,350)#Kw hours
-    Water = random.randint(4310,17000)#Liters
-    
-    meter_readings = [Date,Occupants,Gas,Electric,Water]
-    
-    return (meter_readings)
-
 
 
 #Currently Readings are Monthly Readings
@@ -51,13 +34,13 @@ def generate_meter_readings():
 def create_household_doc():
     
     household_document = {
+        "_id": uuid.uuid4().hex,            #Generates a random unique identifier converted to a hex string
         "occupants": random.randint(1, 6),
         "house_number": random.randint(1, 100),  # Random house number
         "road": fake.street_name(),
         "work_from_home": fake.boolean(),  # Random True/False for work from home
         "readings": generate_meter_readings()  # List of daily readings
     }
-    
     
     return household_document
 
@@ -66,8 +49,13 @@ def create_household_doc():
 
 def main(i):
     
+    households = [create_household_doc() for _ in range(100)]
+    
     doc = create_household_doc()
-    return doc
+    
+    households_collection.insert_many(households)
+    
+    return households
 
 if __name__ == "__main__":
     print(main(1))
